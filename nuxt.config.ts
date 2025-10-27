3600c682b262ddee0aa552b0d13641121e894e90
mathieusteinmetz <mathieu.steinmetz@orange.fr>
2025-10-27 22:51:25 +0100

    Removed unused dependencies and modules: `@nuxt/icon`, `@nuxt/test-utils`, and `better-sqlite3`. Adjusted `nuxt.config.ts`.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/test-utils', '@nuxt/icon'],
+  modules: [],
   ssr: true,
   nitro: {
       preset: 'firebase_app_hosting'
