6c2eaa96ebf6b60b45e8e00fbb0315a8bcdcd7bd
mathieusteinmetz <mathieu.steinmetz@orange.fr>
2025-10-27 22:25:51 +0100

    :sparkles: fix: Enable SSR support and remove '@nuxt/content' module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/test-utils', '@nuxt/icon', '@nuxt/content']
+  modules: ['@nuxt/test-utils', '@nuxt/icon'],
+  ssr: true
 })
